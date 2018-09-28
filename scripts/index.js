669a8a2e012a8f4e6a95e3696a02d912b3a51eae
Pablo Henrique <pablohenrique.r.c@gmail.com>
2018-09-28 01:16:35 -0300

    Adiciona correções

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,7 @@
-$('#btnCount8').click(function() {
+$('#btnPolicy').click(function() {
+  window.location='policy.html';
+});
+
+$('#btnRanking').click(function() {
   window.location='ranking.html';
 });
